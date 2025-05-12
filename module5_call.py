40912dcf5452f27fde2df4269659706006da5707
prekshya-lab1 <prekshyabhatta0@gmail.com>
2025-05-11 23:15:20 -0700

    module5_call.py

--- module5_call.py
+++ module5_call.py
@@ -1,31 +1,10 @@
 from module5_mod import NumberProcessor
 def main():
-  processor = NumberProcessor()
-  While True:
-  try:
+    processor = NumberProcessor()
     n = int(imput("Enter a positive integer N:"))
-    if n<= 0:
-      print("N must be positive.")
-      continue
-    break
-  except ValueError:
-    print("Please enter a valid integer.")
-  for i in range(n):
-    while True:
-      try:
-        num = int(input(f"Enter number {i+1}:"))
-        processor.insert_number(num)
-        break
-      except ValueError:
-        print("Please enter a valid integer.")
-      while True:
-        try:
-          x = int(input("Enter the number X to search for: "))
-        break
-      except ValueError:
-         print("Please enter a valid integer.")
-      index = processor.search_number(x)
-      print(index)
-
-if__name__ == "__main__":
-main()
+    processor.insert_numbers(n) 
+    x = int(input("Enter the number to search for: "))
+    result = processor.find.index(x)
+    print("Index:" if result != -1 else "Not found:", result)
+  if __name__ =="__main__":
+    main()
